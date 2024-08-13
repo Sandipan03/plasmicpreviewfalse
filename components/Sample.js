b0cd67c55c124c10ef2e942e441971a9916996b1
Sandipan03 <sandipan2018@yahoo.com>
2024-08-13 15:40:39 +0530

    c5

--- components/Sample.js
+++ components/Sample.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 function Sample({className}) {
   return (
-    <div className={className}>This is an imported  component</div>
+    <div className={className}><h1>Hello World</h1></div>
   )
 }
 
