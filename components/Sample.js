9a528c8440c2ac2d08820e88805d395ef96c65a5
Sandipan03 <sandipan2018@yahoo.com>
2024-08-13 17:42:25 +0530

    c10

--- components/Sample.js
+++ components/Sample.js
@@ -1,11 +1,15 @@
 /**@format */
-import React from 'react' 
-import Spline from '@splinetool/react-spline';
+import React, { Suspense } from "react";
+const Spline = React.lazy(() => import("@splinetool/react-spline"));
 function Sample({className}) {
   return (
-    <div className={className} style={{ width: "100vw", height: "100vh" }}>
+    
+      <div className={className} style={{ width: "100vw" }}>
+        <Suspense fallback={<h1>Loading...</h1> }>
         <Spline scene='https://prod.spline.design/6Re8UFR1WXy53RFH/scene.splinecode' />
+        </Suspense>
     </div>
+    
   )
 }
 
