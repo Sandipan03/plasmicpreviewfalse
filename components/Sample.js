471ffbf432ffdeed5f60c249d03dc71329399e13
Sandipan03 <sandipan2018@yahoo.com>
2024-08-13 16:24:23 +0530

    c8

--- components/Sample.js
+++ components/Sample.js
@@ -1,6 +1,6 @@
 /**@format */
 import React from 'react'
-
+import Spline from '@splinetool/react-spline';
 function Sample({className}) {
   return (
     <div className={className}><Spline scene="https://prod.spline.design/tJgdrOWwx3zxIXXy/scene.splinecode" /></div>
