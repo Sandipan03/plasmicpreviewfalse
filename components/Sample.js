b8ff4f337231d0490dbebd9b1906e51f1e2d92a7
Sandipan03 <sandipan2018@yahoo.com>
2024-08-14 17:05:02 +0530

    c19

--- components/Sample.js
+++ components/Sample.js
@@ -64,10 +64,10 @@ function Sample({className}) {
   return (
     
       <div className={className} style={{ width: "100vw" , display:"flex", flexDirection:"column", justifyContent:"center", alignItems:"center"}}>
-        <h1 style={{textAlign: "center", color: "white", zIndex:"1"}}>Hello World</h1>
-        <button style={{backgroundColor:"white", color:"black",borderRadius:"5px", height:"30px", marginBottom: "100px",zIndex:"1"}} onClick={()=>{setmodel(true)}}>Open</button>
+        <h1 style={{textAlign: "center", color: "white"}}>Hello World</h1>
+        <button style={{backgroundColor:"white", color:"black",borderRadius:"5px", height:"30px", marginBottom: "100px"}} onClick={()=>{setmodel(true)}}>Open</button>
         {/* <Suspense fallback={<div></div>}> */}
-        {model && <Spline scene='https://prod.spline.design/6Re8UFR1WXy53RFH/scene.splinecode' style={{width:"100%", height: "100vh", position:"absolute", zIndex:"0"}}/>}
+        {model && <Spline scene='https://prod.spline.design/6Re8UFR1WXy53RFH/scene.splinecode' style={{width:"100%", height: "100vh", position:"absolute"}}/>}
         {/* </Suspense> */}
     </div>
     
