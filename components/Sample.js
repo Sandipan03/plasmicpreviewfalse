614534b8691b4e47d7c392b12811d513ec91bd00
Sandipan03 <sandipan2018@yahoo.com>
2024-08-14 12:55:19 +0530

    c14

--- components/Sample.js
+++ components/Sample.js
@@ -23,9 +23,9 @@ function Sample({className}) {
     
       <div className={className} style={{ width: "100vw" , display:"flex", flexDirection:"column", justifyContent:"center", alignItems:"center"}}>
         <h1 style={{textAlign: "center"}}>Hello World</h1>
-        <button style={{backgroundColor:"black", color:"white",borderRadius:"5px", height:"30px", marginBottom: "200px"}} onClick={()=>{setmodel(!model)}}>Open</button>
+        <button style={{backgroundColor:"black", color:"white",borderRadius:"5px", height:"30px", marginBottom: "100px"}} onClick={()=>{setmodel(!model)}}>Open</button>
         <Suspense fallback={<h1>Loading...</h1> }>
-        {model && <Spline scene='https://prod.spline.design/6Re8UFR1WXy53RFH/scene.splinecode' />}
+        {model && <Spline scene='https://prod.spline.design/6Re8UFR1WXy53RFH/scene.splinecode' style={{width:"50%", height: "50%"}}/>}
         </Suspense>
     </div>
     
